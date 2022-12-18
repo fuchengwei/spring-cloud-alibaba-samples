814b6cc0a695e7843f11cd06d832b00a44c93a7b
chengwei.fu <1572060320@qq.com>
2022-12-18 10:54:16 +0800

    :sparkles: 添加权限类型枚举

--- spring-cloud-common/src/main/java/com/github/samples/enums/PermissionTypeEnum.java
+++ spring-cloud-common/src/main/java/com/github/samples/enums/PermissionTypeEnum.java
@@ -17,7 +17,9 @@ public enum PermissionTypeEnum {
     /**
      * 权限类型枚举
      */
-    TYPE_DIRECTORY(1, "目录"), TYPE_MENU(2, "菜单"), TYPE_BUTTON(3, "按钮");
+    TYPE_DIRECTORY(1, "目录"),
+    TYPE_MENU(2, "菜单"),
+    TYPE_BUTTON(3, "按钮");
 
     private final Integer code;
 
@@ -26,7 +28,9 @@ public enum PermissionTypeEnum {
     private static final Map<Integer, PermissionTypeEnum> permissionTypeMap = new HashMap<>();
 
     static {
-        Arrays.stream(PermissionTypeEnum.values()).forEach(permissionTypeEnum -> permissionTypeMap.put(permissionTypeEnum.code, permissionTypeEnum));
+        Arrays
+                .stream(PermissionTypeEnum.values())
+                .forEach(permissionTypeEnum -> permissionTypeMap.put(permissionTypeEnum.code, permissionTypeEnum));
     }
 
     public static PermissionTypeEnum getPermissionTypeEnumByCode(Integer code) {
